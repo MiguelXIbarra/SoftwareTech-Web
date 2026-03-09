3cde0ea8dd95f2f9f9a30c295babc2efb3a67709
MiguelXIbarra <2123100480@soy.utj.edu.mx>
2026-03-09 00:01:14 -0600

    Home redirection

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
